c67d028376a0ecb8241250e298568b5296e2673c
Baruch Even <baruch@ev-en.org>
2013-10-30 23:36:15 +0200

    Fix NTFS definitions to work on all arches
    Patch from Debian: 01-conglomeration.patch

--- src/gm_ntfs.h
+++ src/gm_ntfs.h
@@ -14,6 +14,9 @@
  *
  */
 
+#include <stdint.h>
+#include <asm/byteorder.h>
+
 #ifndef _GM_NTFS_H
 #define _GM_NTFS_H
 
@@ -29,32 +32,31 @@
 /* 'NTFS' in little endian */
 #define NTFS_SUPER_MAGIC	0x5346544E
 
-#if defined(i386) || defined(__i386__) || defined(__alpha__) || defined(__x86_64__)
-
 /* unsigned integral types */
 #ifndef NTFS_INTEGRAL_TYPES
 #define NTFS_INTEGRAL_TYPES
-typedef unsigned char		ntfs_u8;
-typedef unsigned short		ntfs_u16;
-typedef unsigned int		ntfs_u32;
-typedef s64_t			ntfs_u64;
+typedef uint8_t      		ntfs_u8;
+typedef uint16_t      		ntfs_u16;
+typedef uint32_t    		ntfs_u32;
+typedef uint64_t            ntfs_u64;
+typedef int8_t              ntfs_s8;
+typedef int16_t             ntfs_s16;
 #endif /* NTFS_INTEGRAL_TYPES */
-#endif /* defined(i386) || defined(__i386__) || defined(__alpha__) || defined(__x86_64__) */
 
 
 /* Macros reading unsigned integers from a byte pointer */
-/* these should work for all little endian machines */
 #define NTFS_GETU8(p)		(*(ntfs_u8*)(p))
-#define NTFS_GETU16(p)		(*(ntfs_u16*)(p))
-#define NTFS_GETU24(p)		(NTFS_GETU32(p) & 0xFFFFFF)
-#define NTFS_GETU32(p)		(*(ntfs_u32*)(p))
-#define NTFS_GETU64(p)		(*(ntfs_u64*)(p))
+#define NTFS_GETU16(p)		((ntfs_u16)__cpu_to_le16(*(ntfs_u16*)(p)))
+#define NTFS_GETU24(p)		((ntfs_u32)NTFS_GETU16(p) | \
+		                     ((ntfs_u32)NTFS_GETU8(((char*)p)+2))<<16)
+#define NTFS_GETU32(p)		((ntfs_u32)__cpu_to_le32(*(ntfs_u32*)(p)))
+#define NTFS_GETU64(p)		((ntfs_u64)__cpu_to_le64(*(ntfs_u64*)(p)))
 
 /* Macros reading signed integers, returning int */
-#define NTFS_GETS8(p)		((int)(*(char*)(p)))
-#define NTFS_GETS16(p)		((int)(*(short*)(p)))
-#define NTFS_GETS24(p)		(NTFS_GETU24(p) < 0x800000 ? (int)NTFS_GETU24(p) :
-
-
+#define NTFS_GETS8(p)		(*(ntfs_s8*)(p))
+#define NTFS_GETS16(p)		((ntfs_s16)__cpu_to_le16(*(ntfs_s16*)(p)))
+#define NTFS_GETS24(p)		(NTFS_GETU24(p) < 0x800000 ? \
+								(int)NTFS_GETU24(p) : \
+								(int)(NTFS_GETU24(p) - 0x1000000))
 
 #endif /* _GM_NTFS_H */
