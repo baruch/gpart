baruch/gpart

commit 9a5fe0f91c4855cf85b2710f3f19b6eb1a6e430a

Fully remove dynamic module support

src/gmodules.c
@@ -56,7 +56,6 @@ void g_mod_delete(g_module *m)
 {
 	if (m)
 	{
-		if (m->m_hd) dlclose(m->m_hd);
 		if (m->m_name) free((void *)m->m_name);
 		free(m);
 		g_count--;
@@ -131,8 +130,6 @@ g_module *g_mod_lookup(int how,char *name)
 	return (m);
 }
 
-
-
 /*
  * preloaded modules
  */
@@ -141,9 +138,15 @@ void g_mod_addinternals()
 {
 	g_module	*m;
 
-#define GMODINS(mod)	if(!(m = g_mod_lookup(GM_INSERT,#mod))->m_hd){		\
-			m->m_init=mod##_init; m->m_term=mod##_term;	\
-			m->m_gfun=mod##_gfun; }
+#define GMODINS(mod)	\
+	do { \
+		m = g_mod_lookup(GM_INSERT,#mod); \
+		if (!m) {		\
+			m->m_init=mod##_init; \
+			m->m_term=mod##_term; \
+			m->m_gfun=mod##_gfun; \
+		} \
+	} while (0);
 
 	/*
 	 * If no weights are given on the command line, the order

src/gmodules.h
@@ -33,7 +33,6 @@ typedef struct g_mod
 	int		(*m_gfun)(disk_desc *,struct g_mod *);
 	float		m_guess;
 	float		m_weight;	/* probability weight */
-	void		*m_hd;		/* dlopen() descriptor */
 	dos_part_entry	m_part;		/* a guessed partition entry */
 	long		m_align;	/* alignment of partition */
 	struct g_mod	*m_next;

commit d482997ae2e5a14938c4104cbfa738c009bc41bf

Fix bug introduced in 9a5fe0f91c4855cf85b2710f3f19b6eb1a6e430a

src/gmodules.c
@@ -141,7 +141,7 @@ void g_mod_addinternals()
 #define GMODINS(mod)	\
 	do { \
 		m = g_mod_lookup(GM_INSERT,#mod); \
-		if (!m) {		\
+		if (m) {		\
 			m->m_init=mod##_init; \
 			m->m_term=mod##_term; \
 			m->m_gfun=mod##_gfun; \